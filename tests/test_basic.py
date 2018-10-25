edadd8c2624437a82e5a99199c13590528513099
James Baster <james.baster@opendataservices.coop>
2018-10-25 12:56:02 +0100

    Tests

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -32,3 +32,16 @@ def test_error_wrong_file_type():
                                                     file_type='json')
     finally:
         shutil.rmtree(cove_temp_folder)
+
+def test_error_bad_version():
+    filename = os.path.join(os.path.dirname(
+        os.path.realpath(__file__)), 'fixtures', 'basic', 'bad_version.json'
+    )
+
+    cove_temp_folder = tempfile.mkdtemp(prefix='ocdsmacaw-tests-', dir=tempfile.gettempdir())
+    try:
+        with pytest.raises(ocdsmacaw.api.APIException):
+            ocdsmacaw.api.ocds_json_output(cove_temp_folder, filename, None, convert=False, cache_schema=True,
+                                                    file_type='json')
+    finally:
+        shutil.rmtree(cove_temp_folder)
